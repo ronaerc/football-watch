5482086191732af59a6d84ff051e04a8f6a202d2
Patrick McCreanor <pmccreanor@can-jlc-qejhd4.loycorp.local>
2020-03-25 09:32:00 -0400

    Updates

---
+++ precache-manifest.646117021864d3a8dfe57e65f09ae47b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653fb0982f8678a15c3fbfc56ba37ec7",
+    "url": "/football-watch/index.html"
+  },
+  {
+    "revision": "3836f43afa6bc846ab15",
+    "url": "/football-watch/static/css/main.a41ad390.chunk.css"
+  },
+  {
+    "revision": "fd60c6b533308f684409",
+    "url": "/football-watch/static/js/2.81ec1a78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/football-watch/static/js/2.81ec1a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3836f43afa6bc846ab15",
+    "url": "/football-watch/static/js/main.8b89959c.chunk.js"
+  },
+  {
+    "revision": "91ec1c9fad0f2f2292bc",
+    "url": "/football-watch/static/js/runtime-main.4bcd343b.js"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/football-watch/static/media/logo-white.40d4ef82.png"
+  }
+]);
