c56251e7962f0b4020b46fffb2583cb0b561baa3
Patrick McCreanor <pmccreanor@can-jlc-qejhd4.loycorp.local>
2020-03-25 09:32:28 -0400

    initial commit

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './button.styles.css';
+
+export const Button = props => (
+    <a href="" className={props.className}>{props.children}</a>
+) 
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Button} from '../button/button.component.jsx';
+
+import logo from '../../logo-white.png';
+
+import './header.styles.css';
+
+export const Header = props => (
+    <header className="header">
+        <div className="header__logo-box">
+            <img src={logo} alt="logo" className="header__logo" />
+        </div>
+
+        <div className="header__text-box">
+            <h1 className="heading-primary">
+                <span className="heading-primary--main">{props.siteName}</span>
+                <span className="heading-primary--sub">{props.siteSubName}</span>
+            </h1>
+
+
+            <Button className="btn btn--black btn--animated">About</Button>
+           
+        </div>
+
+    </header>
+) 
